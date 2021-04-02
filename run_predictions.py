1b3d553907a8974a81008d7fbe1bb0c5ac5b0e29
ilyasher <ilyasherstyuk@gmail.com>
2021-04-01 22:54:04 -0700

    Make bounding boxes gobble up smaller ones

--- run_predictions.py
+++ run_predictions.py
@@ -7,7 +7,7 @@ templates_dir = './templates/red-light'
 template_files = sorted(os.listdir(templates_dir))
 templates = [np.load(os.path.join(templates_dir, f)) for f in template_files if 'template' in f]
 
-def detect_red_light(I):
+def detect_red_light(I, visualize=False):
     '''
     This function takes a numpy array <I> and returns a list <bounding_boxes>.
     The list <bounding_boxes> should have one element for each red light in the
@@ -24,81 +24,102 @@ def detect_red_light(I):
 
     bounding_boxes = [] # This should be a list of lists, each of length 4. See format example below.
 
-    ''' BEGIN YOUR CODE '''
-
-    '''
-    As an example, here's code that generates between 1 and 5 random boxes
-    of fixed size and returns the results in the proper format.
-    '''
-
     cpy = I.copy()
+
+    # Convert to float so that we can normalize
     I = I.astype(np.float32)
 
-    # Subtract out average
+    # Normalize image
+    # It would *probably* be better to normalize with respect
+    # to the rest of the dataset instead.
     mean = np.mean(I, axis=(0, 1))
     std  = np.std(I, axis=(0, 1))
     I = (I - mean) / std
 
+    # Resize a np.array to the given width and height
     def resize(arr, w, h):
         shape = list(arr.shape)
-        shape[:2] = [w, h]
+        shape[:2] = [h, w]
         ret = np.zeros(shape)
-        old_w, old_h = arr.shape[:2]
+        old_h, old_w = arr.shape[:2]
         for x in range(w):
             for y in range(h):
                 old_x = int(x / w * old_w)
                 old_y = int(y / h * old_h)
-                ret[x][y] = arr[old_x][old_y]
+                ret[y][x] = arr[old_y][old_x]
         return ret
 
-    def make_templates_sizes(max_size):
-        template_sizes = list()
-        size = 4
-        while size < max_size:
-            template_sizes.append(int(size))
-            size *= 1.5
-        return [4, 6, 9, 15, 24, 36, 54, 72]
-        return template_sizes
+    # Return a list of template side lengths that we will try
+    # to match in the image.
+    def make_templates_sizes():
+        # I used to have something more complicated but
+        # it's not really necessary for this assignment
+        return [6, 9, 15, 24, 36, 54, 72]
 
+    # Return (distinct templates) cross (side lengths)
     def make_templates_sized(templates):
         templates_sized = list()
-        sizes = make_templates_sizes(min(I.shape[:2]))
-        for template in templates:
-            for size in sizes:
+        sizes = make_templates_sizes()
+        for size in sizes:
+            for template in templates:
                 templates_sized.append(resize(template, size, size))
         return templates_sized
 
-    w, h, _ = I.shape
-    for template in make_templates_sized(templates[:2]):
+    # consider resizing image instead
+
+    h, w, _ = I.shape
+    for template in make_templates_sized(templates):
+
         kernel_size = template.shape[0]
         stride = int(kernel_size / 5) + 1
+
+        # The dot product above which we accept the detection.
         threshold = 2.5 * (kernel_size ** 2)
+
         for x in range(0, w-kernel_size, stride):
             for y in range(0, h-kernel_size, stride):
-                target = I[x:x+kernel_size, y:y+kernel_size, :]
+
+                # Patch of the original image
+                target = I[y:y+kernel_size, x:x+kernel_size, :]
+
+                # How similar it is to the template
                 dot = np.sum(target * template)
+
+                # Object detected
                 if dot > threshold:
-                    print(x, y, int(dot), dot / (kernel_size ** 2))
-                    cpy[x:x+kernel_size, y:y+kernel_size, 1] = 128
 
-    Image.fromarray(cpy).show()
+                    # Sorry for this illegible mess
+                    # This is me trying to deal with overlapping boxes.
+                    # If the old box's center is within this box,
+                    #   we eat the old box.
+                    new_box = [y, x, y+kernel_size-1, x+kernel_size-1]
+                    old_bounding_boxes = bounding_boxes.copy()
+                    for i, box in enumerate(old_bounding_boxes):
+                        tl_y, tl_x, br_y, br_x = tuple(box)
+                        old_center_y = int((tl_y + br_y) / 2)
+                        old_center_x = int((tl_x + br_x) / 2)
+                        if old_center_y >= y and old_center_y <= new_box[2] and\
+                           old_center_x >= x and old_center_x <= new_box[3]:
+                            new_box = [min(tl_y, y), min(tl_x, x),
+                                       max(br_y, new_box[2]),
+                                       max(br_x, new_box[3])]
+                            bounding_boxes.remove(box)
+                    bounding_boxes.append(new_box)
 
-    """
-    box_height = 8
-    box_width = 6
+    # Returns a green outline to be pasted on top of detections
+    def make_outline(h, w):
+        outline = np.zeros(shape=(h, w, 3))
+        outline[:, :, 1] = 255
+        outline[2:-2, 2:-2, 1] = 0
+        return outline
 
-    num_boxes = np.random.randint(1,5)
-
-    for i in range(num_boxes):
-        (n_rows,n_cols,n_channels) = np.shape(I)
-
-        tl_row = np.random.randint(n_rows - box_height)
-        tl_col = np.random.randint(n_cols - box_width)
-        br_row = tl_row + box_height
-        br_col = tl_col + box_width
-
-        bounding_boxes.append([tl_row,tl_col,br_row,br_col])
-    """
+    if visualize:
+        for box in bounding_boxes:
+            y0, x0, y1, x1 = tuple(box)
+            cutout = cpy[y0:y1+1, x0:x1+1, :]
+            cutout = np.maximum(cutout, make_outline(y1-y0+1, x1-x0+1))
+            cpy[y0:y1+1, x0:x1+1, :] = cutout
+        Image.fromarray(cpy).show()
 
     ''' END YOUR CODE '''
 
@@ -121,7 +142,7 @@ file_names = sorted(os.listdir(data_path))
 file_names = [f for f in file_names if '.jpg' in f]
 
 preds = {}
-for file_name in file_names[:10]:
+for file_name in file_names[100:110]:
 
     # read image using PIL:
     I = Image.open(os.path.join(data_path, file_name))
@@ -129,7 +150,7 @@ for file_name in file_names[:10]:
     # convert to numpy array:
     I = np.asarray(I)
 
-    preds[file_name] = detect_red_light(I)
+    preds[file_name] = detect_red_light(I, visualize=True)
 
 # save preds (overwrites any previous predictions!)
 with open(os.path.join(preds_path,'preds.json'),'w') as f:
